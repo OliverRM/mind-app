c6c28024011e442dbfa34209f7aed8bccacab905
Oliver Mayer <git@oliverrm.de>
2023-09-21 12:59:39 +0200

    Install tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.tsx"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: ['"Source Sans 3 VF"', "sans-serif"],
+    },
+  },
+  plugins: [],
+};
