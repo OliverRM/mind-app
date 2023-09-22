09e1f8af2ed6885e5a92018876bf47563b00413f
Oliver Mayer <git@oliverrm.de>
2023-09-22 11:12:57 +0200

    Install capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'de.mindakademie.app',
+  appName: 'Mind Akademie',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
